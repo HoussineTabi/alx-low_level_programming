38ae87792bc531d5a068898eed6435020c0c4ffc
HoussineTabi <0620036304ht@gmail.com>
2023-08-17 17:25:10 +0000

    tasks

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,7 +11,7 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *head = *h, *node = NULL, prev = NULL;
+	dlistint_t *head = *h, *node = NULL, *prev = NULL;
 	unsigned int i = 0;
 
 	if (!h)
