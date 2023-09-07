3cb1ac4d643746c026a2cdb3b2a8163c4ae9a87a
HoussineTabi <0620036304ht@gmail.com>
2023-09-07 23:01:57 +0000

    task4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,20 @@
+#include "hash_tables.h"
+/**
+ * char *hash_table_get - gets an element in a hash table
+ * @ht: the hash table
+ * @key: the key of the to find the element
+ *
+ * Return: the value of the key exist and NULL if not
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+
+	if (!key)
+		return (NULL);
+	index = key_index((unsigned char *)key, ht->size);
+	if (ht->array[index] == NULL)
+		return (NULL);
+	return (ht->array[index]->value);
+}
