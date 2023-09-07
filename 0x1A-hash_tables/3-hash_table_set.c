9ab421f47d80c540ae660e94823ae32fd5e0bb3d
HoussineTabi <0620036304ht@gmail.com>
2023-09-07 22:44:55 +0000

    task3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -16,9 +16,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (!ht || !key)
 		return (0);
-	node = (void *)malloc(sizeof(hash_node_t));
+	node = malloc(sizeof(hash_node_t));
 	if (!node)
-		return (NULL);
+		return (0);
 	node->key = strdup(key);
 	node->value = strdup(value);
 	node->next = NULL;
