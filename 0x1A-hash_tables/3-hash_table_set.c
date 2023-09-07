fc394c1a3cce559a576e9aceac3232bfd6851d35
HoussineTabi <0620036304ht@gmail.com>
2023-09-07 22:29:28 +0000

    task3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,13 +14,13 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *node_at_end = NULL;
 	unsigned long int index;
 
-	if (!ht || !key || !value)
+	if (!key)
 		return (0);
 	node.key = (char *)strdup(key);
 	node.value = (char *)strdup(value);
 	node.next = NULL;
 	index = key_index((unsigned char *)key, ht->size);
-	if (!ht->array[index])
+	if (ht->array[index] == NULL)
 		ht->array[index] = &node;
 	else
 	{
