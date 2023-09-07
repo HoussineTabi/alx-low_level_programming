64804c2d9aaa18a795d329457b036ef9ae07eccb
HoussineTabi <0620036304ht@gmail.com>
2023-09-07 18:10:09 +0000

    tassk

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,13 +10,14 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *table = NULL;
 	unsigned long int count;
 
-	table = malloc(sizeof(hash_table_t) * size);
+	table = malloc(sizeof(hash_table_t));
 	if (!table)
 		return (NULL);
+	table->array = malloc(sizeof(hash_node_t *) * size);
+	if (!table->array)
+		return (NULL);
 	for (count = 0; count < size; count++)
-	{
-		table->size = 0;
-		table->array = NULL;
-	}
+		table->array[count] = NULL;
+	table->size = size;
 	return (table);
 }
