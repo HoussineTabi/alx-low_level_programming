01a05c9a0ea4e348b011083f3beafad186366c74
HoussineTabi <0620036304ht@gmail.com>
2023-09-08 10:09:04 +0000

    task_free_hash_table

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -10,7 +10,7 @@ void hash_table_print(const hash_table_t *ht)
 	hash_node_t *node = NULL;
 	int check = 0;
 
-	if (ht != NULL || ht->array)
+	if (ht != NULL)
 	{
 		printf("{");
 		if (ht->array && ht->size)
