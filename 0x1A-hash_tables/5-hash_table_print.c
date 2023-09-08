4fe561de4900d764f3b4d6397ef4e15710c6a7a6
HoussineTabi <0620036304ht@gmail.com>
2023-09-08 00:00:03 +0000

    task5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - prints the element of a hash table
+ * @ht: the hash table to print
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int count = 0;
+	hash_node_t *node = NULL;
+
+	if (ht && ht->array && ht->size)
+	{
+		printf("{");
+		for (; count < ht->size; count++)
+		{
+			if (count && ht->array[count])
+				printf(", ");
+			if (ht->array[count])
+			{
+				node = ht->array[count];
+				while (node->next)
+				{
+					printf("'%s': '%s', ", node->key, node->value);
+					node = node->next;
+				}
+				printf("'%s': '%s'", node->key, node->value);
+			}
+		}
+		printf("}\n");
+	}
+}
