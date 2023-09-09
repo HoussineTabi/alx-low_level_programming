d6a1857655e91370a82675c18efaf892371b9acd
HoussineTabi <0620036304ht@gmail.com>
2023-09-09 11:04:36 +0000

    taskoptionaly

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -41,7 +41,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			}
 		}
 		if (node_at_end->next == NULL && strcmp(node_at_end->key, key))
-			node->next = ht->array[index], ht->array[index] = node;
+			node_at_end->next = node;
 		else
 		{
 			free(node), free(node_at_end->value);
